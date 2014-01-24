61d4ff42a75a69e9bb02872337219de744cb561a
Andrea Panattoni <panattoni.andrea@gmail.com>
2014-01-24 18:41:23 +0100

    AppContext singleton instance

--- src/laborra/ioc/AppContext.php
+++ src/laborra/ioc/AppContext.php
@@ -14,12 +14,28 @@ class AppContext implements IBeanFactory, IParametersHolder
     /** IParametersHolder $paramHolder */
     private $paramHolder;
 
-    function __construct (
+    /** @var  AppContext */
+    private static $context;
+
+    public function __construct (
         IParametersHolder $paramHolder,
         IBeanFactory $beanFactory)
     {
         $this->paramHolder = $paramHolder;
         $this->beanFactory = $beanFactory;
+    }
+
+    public static function init (AppContext $context)
+    {
+        self::$context = $context;
+    }
+
+    /**
+     * @return AppContext
+     */
+    public static function get ()
+    {
+        return self::$context;
     }
 
     public function getBean ($beanId)
@@ -31,4 +47,5 @@ class AppContext implements IBeanFactory, IParametersHolder
     {
         return $this->paramHolder->getParameter($paramName);
     }
+
 }
